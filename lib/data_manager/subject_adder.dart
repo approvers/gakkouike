b223d43bfa945b4e0df803dfb286e80157676dcc
loxygenk <loxygen.k@gmail.com>
2020-02-28 22:31:27 +0900

    [Add] Created much simple subject add screen

--- lib/data_manager/subject_adder.dart
+++ lib/data_manager/subject_adder.dart
@@ -1,12 +1,102 @@
 import 'package:flutter/material.dart';
 
+import '../subject.dart';
+
 class SubjectAdder extends StatelessWidget{
   @override
+  Widget build(BuildContext context) => _SubjectAdder();
+}
+
+class _SubjectAdder extends StatelessWidget {
+
+  @override
   Widget build(BuildContext context) {
+
+    final nameTextController = new TextEditingController();
+    final numberTextController = new TextEditingController();
+
     return Scaffold(
-      appBar: AppBar(title: Text("教科追加"),),
-      body: Center(
-        child: Text("だめです"),
+      appBar: AppBar(title: Text("教科を追加する"),),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(fontSize: 18),
+                labelText: "教科名",
+                hintText: "欠課した教科の名前を入力してください"
+              ),
+              controller: nameTextController,
+            ),
+            Container(
+              margin: EdgeInsets.all(10)
+            ),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelStyle: TextStyle(fontSize: 18),
+                labelText: "予定されている授業数",
+                hintText: "数字で入力をしましょう"
+              ),
+              controller: numberTextController,
+            ),
+            Container(
+              margin: EdgeInsets.all(20)
+            ),
+            RaisedButton(
+              child: Text("追加する"),
+              shape: UnderlineInputBorder(),
+              // ignore: missing_return
+              onPressed: (){
+                if(nameTextController.text == "" || numberTextController.text == ""){
+                  return showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: Text("エラー"),
+                        content: Text("必要なデータが入力されていません！"),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("了解"),
+                            onPressed: () => Navigator.pop(context),
+                          )
+                        ],
+                      );
+                    }
+                  );
+                }
+                if(int.tryParse(numberTextController.text) == null) {
+                  return showDialog(
+                    context: context,
+                    builder: (_) {
+                      return AlertDialog(
+                        title: Text("エラー"),
+                        content: Text("予定されている授業数に数値以外の何かが入力されています！"),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("了解"),
+                            onPressed: () => Navigator.pop(context),
+                          )
+                        ],
+                      );
+                    }
+                  );
+                }
+
+                // TODO: ストレージに書き込む
+                Subject generated = new Subject(
+                    nameTextController.text,
+                    int.parse(numberTextController.text)
+                );
+                print(generated.name);
+                Navigator.pop(context);
+              }
+            )
+          ],
+        ),
       ),
     );
   }
